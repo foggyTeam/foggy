0c1b98717f53e83ed14921256f40587e0dde31c5
IciIcifur <iciicifur@yandex.ru>
2024-12-28 23:45:14 +0300

    Better fetch & architecture
    - added MobX for states
    - added SWR for better fetches
    ***
    frontend

--- frontend/app/(pages)/login/page.tsx
+++ frontend/app/(pages)/login/page.tsx
@@ -1,47 +1,60 @@
 "use client";
 
-import React, { use, useEffect } from "react";
+import React from "react";
 import { Button, Form, Input } from "@nextui-org/react";
-import { addNewPet, getPetById } from "@/app/utils/services/loginService";
+import petStore from "@/app/stores/petStore";
+import { observer } from "mobx-react-lite";
+import { useCurrentPet } from "@/app/hooks/useCurrentPet";
 
-export default function Login(data) {
-  const [submitted, setSubmitted] = React.useState(null);
-  addNewPet("Samantha", "dog", 5).finally();
-  getPetById(5).then((pet) => console.log(pet.name, pet.category.name, pet.id));
+const Login = React.memo(
+  observer(() => {
+    const [submitted, setSubmitted] = React.useState(null);
+    const id = 5;
 
-  const onSubmit = (e) => {
-    e.preventDefault();
+    const { data, error } = useCurrentPet(id);
 
-    const data = Object.fromEntries(new FormData(e.currentTarget));
+    if (error) {
+      return <div>{petStore.error}</div>;
+    }
 
-    setSubmitted(data);
-  };
+    const onSubmit = (e) => {
+      e.preventDefault();
 
-  return (
-    <div className={"flex justify-center m-64"}>
-      <Form
-        className="w-full max-w-xs"
-        validationBehavior="native"
-        onSubmit={onSubmit}
-      >
-        <Input
-          isRequired
-          errorMessage="Please enter a valid email"
-          label="Email"
-          labelPlacement="outside"
-          name="email"
-          placeholder="Enter your email"
-          type="email"
-        />
-        <Button type="submit" variant="bordered" color="primary">
-          Submit
-        </Button>
-        {submitted && (
-          <div className="text-small text-default-500">
-            You submitted: <code>{JSON.stringify(submitted)}</code>
-          </div>
-        )}
-      </Form>
-    </div>
-  );
-}
+      const data = Object.fromEntries(new FormData(e.currentTarget));
+
+      setSubmitted(data);
+    };
+
+    return (
+      <div className={"flex justify-center m-64"}>
+        <Form
+          className="w-full max-w-xs"
+          validationBehavior="native"
+          onSubmit={onSubmit}
+        >
+          <Input
+            isRequired
+            errorMessage="Please enter a valid email"
+            label="Email"
+            labelPlacement="outside"
+            name="email"
+            placeholder={
+              petStore.currentPet ? petStore.currentPet.name : "Pet name..."
+            }
+            type="email"
+          />
+          <Button type="submit" variant="bordered" color="primary">
+            Submit
+          </Button>
+          {submitted && (
+            <div className="text-small text-default-500">
+              You submitted: <code>{JSON.stringify(submitted)}</code>
+            </div>
+          )}
+        </Form>
+      </div>
+    );
+  }),
+);
+
+export default Login;
