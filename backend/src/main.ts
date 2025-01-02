9a06dbfcf6a5c31ad993fccb2fa88dd717b4aafa
MINTCanella <veklebes@gmail.com>
2025-01-02 20:47:52 +0300

    FOGGY-3 Enable Cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,13 +4,23 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: 'GET,POST,PUT,PATCH,DELETE,HEAD,OPTIONS',
+    credentials: true,
+  });
+
   const config = new DocumentBuilder()
-      .setTitle('Foggy API Documentation')
-      .setDescription('The API description')
-      .setVersion('1.0')
-      .build();
+    .setTitle('Foggy API Documentation')
+    .setDescription('The API description')
+    .setVersion('1.0')
+    .build();
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+
   await app.listen(3001);
 }
+
 bootstrap();
