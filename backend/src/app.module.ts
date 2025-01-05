999f3e813e63a7696d53ef0672942474c7a87287
IciIcifur <iciicifur@yandex.ru>
2025-01-05 14:43:04 +0300

    Docker works
    - set up backend .env
    - frontend fails with InternalServerError (potentially errors while building an app)
    
    ***
    frontend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,10 +3,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
+import { env } from 'process';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/foggydatabase'),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(
+      env.NODE_ENV === 'development'
+        ? env.MONGO_URI_LOCAL
+        : env.MONGO_URI_DOCKER,
+    ),
     UsersModule,
   ],
   controllers: [AppController],
