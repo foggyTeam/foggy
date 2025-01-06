4940b2f4fb2a9f20f68d42d50fb248cd47467464
IciIcifur <iciicifur@yandex.ru>
2025-01-06 21:10:40 +0300

    Environment variables finally work
    ***
    frontend

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,12 +1,6 @@
-import type { NextConfig } from 'next';
-import dotenv from 'dotenv';
-import path from 'path';
+import { NextConfig } from 'next';
+import './envConfig.ts';
 
-dotenv.config({ path: path.resolve(__dirname, '.env') });
-
-const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  env: { NEXT_PUBLIC_API_URI: process.env.NEXT_PUBLIC_API_URI },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
