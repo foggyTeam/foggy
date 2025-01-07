428f8e83e51b15896468b00e3d535c3f663d4d8c
IciIcifur <iciicifur@yandex.ru>
2025-01-08 00:28:53 +0300

    Some linter's fixes
    ***
    frontend

--- frontend/app/(pages)/login/page.tsx
+++ frontend/app/(pages)/login/page.tsx
@@ -5,7 +5,7 @@ import LoginForm from '@/app/lib/components/loginForm';
 
 const Login = () => {
   return (
-    <div className={'flex justify-center items-center w-screen h-screen'}>
+    <div className={'flex h-screen w-screen items-center justify-center'}>
       <LoginForm />
     </div>
   );
