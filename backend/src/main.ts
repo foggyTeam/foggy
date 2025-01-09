5096fcdc268b54af9129676c8c8139fa6c579492
MINTCanella <veklebes@gmail.com>
2025-01-09 14:03:15 +0300

    Обновление схемы и dto, добавление validationPipe Добавление вывода ошибок

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({
+    transform: true,
+    whitelist: true,
+    forbidNonWhitelisted: true,
+  }));
 
   const configService = app.get(ConfigService);
 
