db41f046440219d4af955ae57454e74117f196c6
IciIcifur <iciicifur@yandex.ru>
2025-01-19 20:42:59 +0300

    Husky fixes smh
    ***
    frontend

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,5 +1,5 @@
 import type { Config } from 'tailwindcss';
-import { heroui } from "@heroui/react";
+import { heroui } from '@heroui/react';
 
 export function to_rgb(hex: string): string {
   let r = 0;
@@ -60,7 +60,7 @@ export default {
     './(pages)/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
-    "../node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
+    '../node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
