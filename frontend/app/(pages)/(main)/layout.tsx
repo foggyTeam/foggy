8138abfd94a0660264fbcf46691798a7b455be34
IciIcifur <iciicifur@yandex.ru>
2025-02-06 12:49:58 +0300

    Start versions of menus
    ***
    frontend

--- frontend/app/(pages)/(main)/layout.tsx
+++ frontend/app/(pages)/(main)/layout.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import LogoBar from '@/app/lib/components/menu/logoBar';
-import SideBar from '@/app/lib/components/menu/sideBar';
+import SideBar from '@/app/lib/components/menu/sideBar/sideBar';
 import UserLoader from '@/app/lib/components/utils/userLoader';
 
 export default function MainLayout({ children }) {
--- frontend/app/lib/utils/style_definitions.ts
+++ frontend/app/lib/utils/style_definitions.ts
@@ -1,8 +1,3 @@
-import { primary } from '@/tailwind.config';
-
 export const bg_container: string =
-  'overflow-clip  border-1 border-white\n' +
-  'border-opacity-10 bg-white bg-opacity-50 px-1 py-12 backdrop-blur-3xl rounded-2xl';
-
-export const sh_container: string =
-  'shadow-[0_8px_5px_' + primary.DEFAULT.toString() + '11]';
+  'overflow-clip  border-1 border-white border-opacity-10 bg-white bg-opacity-50' +
+  ' px-1 py-12 backdrop-blur-3xl rounded-2xl shadow-container';
