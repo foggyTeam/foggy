078f5db2a902c5e65a7b4be894769e8d0dc85b4c
IciIcifur <iciicifur@yandex.ru>
2025-03-02 17:15:40 +0300

    Added starter color picker
    - in progress
    
    ***
    frontend

--- frontend/app/lib/components/board/tools/fillTool.tsx
+++ frontend/app/lib/components/board/tools/fillTool.tsx
@@ -3,6 +3,11 @@ import { Button } from '@heroui/button';
 import { useEffect, useState } from 'react';
 import { info, to_rgb } from '@/tailwind.config';
 import { BoardElement } from '@/app/lib/types/definitions';
+import { Popover, PopoverContent, PopoverTrigger } from '@heroui/popover';
+import settingsStore from '@/app/stores/settingsStore';
+import { HslaColorPicker } from 'react-colorful';
+import clsx from 'clsx';
+import { bg_container_no_padding } from '@/app/lib/types/styles';
 
 export default function FillTool({ element, updateElement }) {
   const [fillColor, changeColor] = useState('#ffffff');
@@ -11,18 +16,36 @@ export default function FillTool({ element, updateElement }) {
   }, [element]);
 
   return (
-    <Button
-      onPress={() => {
-        updateElement(element.attrs.id, {
-          fill: info.DEFAULT,
-        } as BoardElement);
-      }}
-      variant="light"
-      color="default"
-      isIconOnly
-      size="md"
-    >
-      <CircleIcon fill={fillColor} stroke={`rgba(${to_rgb(fillColor)}, .48)`} />
-    </Button>
+    <Popover>
+      <PopoverTrigger>
+        <Button
+          onPress={() => {
+            updateElement(element.attrs.id, {
+              fill: info.DEFAULT,
+            } as BoardElement);
+          }}
+          variant="light"
+          color="default"
+          isIconOnly
+          size="md"
+        >
+          <CircleIcon
+            fill={fillColor}
+            stroke={`rgba(${to_rgb(fillColor)}, .48)`}
+          />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent
+        className={clsx(
+          bg_container_no_padding,
+          'flex flex-col gap-1 p-2 sm:p-3',
+        )}
+      >
+        <h3 className="w-full justify-start text-small font-medium text-default-800">
+          {settingsStore.t.toolBar.chooseColor}
+        </h3>
+        <HslaColorPicker className="gap-1 rounded-md" />
+      </PopoverContent>
+    </Popover>
   );
 }
