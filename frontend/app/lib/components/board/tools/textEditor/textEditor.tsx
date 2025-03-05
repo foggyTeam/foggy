559af4d8f6cb5ab0f5a507ad091b5cd42bf162aa
IciIcifur <iciicifur@yandex.ru>
2025-03-06 02:34:12 +0300

    Starter text editor
    - bold, italic, struck, underlined, quote
    - header 1, header 2, header 3
    
    ***
    frontend

--- frontend/app/lib/components/board/tools/textEditor/textEditor.tsx
+++ frontend/app/lib/components/board/tools/textEditor/textEditor.tsx
@@ -1,59 +1,50 @@
-import React from 'react';
-import ReactQuill from 'react-quill-new';
+import React, { useEffect, useRef, useState } from 'react';
+import 'quill/dist/quill.core.css';
+import '@/app/lib/components/board/tools/textEditor/quillOverrides.css';
+import Quill from 'quill';
 import settingsStore from '@/app/stores/settingsStore';
 import TextEditorToolBar from '@/app/lib/components/board/tools/textEditor/textEditorToolBar';
 
-const quillModules = {
-  toolbar: {
-    container: '#toolbar',
-    handlers: {
-      // Добавьте свои обработчики, если необходимо
-    },
-    /*
-    * ['bold', 'italic', 'underline', 'strike', 'blockquote', 'link'],
-    [{ header: [1, 2, 3, false] }],
-    [{ color: [] }, { background: [] }, { font: [montserrat] }],
-    [{ align: [] }, { list: 'ordered' }, { list: 'bullet' }, { list: 'check' }],
-    ['clean'],
-    * */
-  },
-};
-
-const quillFormats = [
-  'header',
-  'font',
-  'bold',
-  'italic',
-  'underline',
-  'strike',
-  'blockquote',
-  'list',
-  'bullet',
-  'check',
-  'link',
-  'color',
-  'background',
-  'align',
-];
-
 export default function CustomTextEditor({ top, left, content, setContent }) {
+  const quillRef = useRef<Quill | null>(null);
+  const editorContainerRef = useRef<HTMLDivElement>(null);
+
+  const [selectionFormat, setSelectionFormat] = useState({} as any);
+
+  useEffect(() => {
+    if (editorContainerRef.current) {
+      const quill: Quill = new Quill(editorContainerRef.current, {
+        theme: 'snow',
+        modules: {
+          toolbar: false,
+        },
+        placeholder: settingsStore.t.toolBar.textToolPlaceholder,
+      });
+
+      quillRef.current = quill;
+
+      quill.on('selection-change', () => {
+        const format = quill.getFormat();
+        setSelectionFormat(format);
+      });
+    }
+  }, []);
+
   return (
     <div
       style={{
         position: 'fixed',
-        top: top - 42.52,
-        left: left,
+        top: top - 65.33 - 16,
+        left: left - 16,
       }}
+      className="max-w-96"
     >
-      <TextEditorToolBar />
-      <ReactQuill
-        value={content}
-        onChange={setContent}
-        modules={quillModules}
-        formats={quillFormats}
-        placeholder={settingsStore.t.toolBar.textToolPlaceholder}
-        className="max-w-96"
+      <TextEditorToolBar
+        selectionFormat={selectionFormat}
+        setSelectionFormat={setSelectionFormat}
+        quillRef={quillRef}
       />
+      <div ref={editorContainerRef} className="quill-editor-container"></div>
     </div>
   );
 }
--- frontend/app/lib/components/board/tools/textEditor/textEditorToolBar.tsx
+++ frontend/app/lib/components/board/tools/textEditor/textEditorToolBar.tsx
@@ -1,49 +1,129 @@
-import ColorPicker from '@/app/lib/components/board/tools/colorPicker';
-import { useState } from 'react';
-import { primary } from '@/tailwind.config';
+import React from 'react';
+import { Button } from '@heroui/button';
+import {
+  BoldIcon,
+  Heading1Icon,
+  Heading2Icon,
+  Heading3Icon,
+  ItalicIcon,
+  LinkIcon,
+  QuoteIcon,
+  StrikethroughIcon,
+  UnderlineIcon,
+} from 'lucide-react';
+import { bg_container_no_padding } from '@/app/lib/types/styles';
+import Quill from 'quill';
+import clsx from 'clsx';
+import { Divider } from '@heroui/divider';
 
-/* TODO:
-Bold: ql-bold
-Italic: ql-italic
-Underline: ql-underline
-Strike: ql-strike
-Blockquote: ql-blockquote
-Link: ql-link
-Header: ql-header (включая уровни заголовков: ql-header-1, ql-header-2, и т.д.)
-Color: ql-color
-Background: ql-background
-Font: ql-font
-Align: ql-align
-List (ordered): ql-list с атрибутом value="ordered"
-List (bullet): ql-list с атрибутом value="bullet"
-List (check): ql-list с атрибутом value="check"
-Clean: ql-clean
-*/
+/*
+TODO:
+[{ list: 'ordered' }, { list: 'bullet' }, { list: 'check' }],
+['link'],
+[{ align: [] }],
+[{ color: [] }, { background: [] }],
+['clean'],
+ */
 
-export default function TextEditorToolBar() {
-  const [color, setColor] = useState(primary.DEFAULT);
+export default function TextEditorToolBar({
+  selectionFormat,
+  setSelectionFormat,
+  quillRef,
+}) {
+  const textEditorTools = [
+    [
+      {
+        id: 'bold',
+        ToolIcon: BoldIcon,
+      },
+      {
+        id: 'italic',
+        ToolIcon: ItalicIcon,
+      },
+      {
+        id: 'underline',
+        ToolIcon: UnderlineIcon,
+      },
+      {
+        id: 'strike',
+        ToolIcon: StrikethroughIcon,
+      },
+      {
+        id: 'blockquote',
+        ToolIcon: QuoteIcon,
+      },
+    ],
+    [
+      { id: 'header-1', ToolIcon: Heading1Icon },
+      { id: 'header-2', ToolIcon: Heading2Icon },
+      { id: 'header-3', ToolIcon: Heading3Icon },
+    ],
+    [{ id: 'link', ToolIcon: LinkIcon }],
+  ];
+
+  const handleClick = (event) => {
+    if (quillRef.current) {
+      const id: string = event.target.id;
+      // если id сложный и содержит значение, отделим его
+      const { clickType, value } = id.includes('-')
+        ? { clickType: id.split('-')[0], value: parseInt(id.split('-')[1]) }
+        : { clickType: id, value: undefined };
+      // получим и обновим данные о выделенном фрагменте из Quill
+      const quill = quillRef.current as Quill;
+      const isType = quill.getFormat()[clickType];
+      const newFormat =
+        value && !isType ? value : isType === value ? !isType : value;
+      quill.format(clickType, newFormat);
+
+      // обновим собственные данные на основе данных Quill
+      setSelectionFormat({
+        ...selectionFormat,
+        [clickType]: newFormat,
+      });
+    }
+  };
 
   return (
-    <div id="toolbar">
-      <select className="ql-font">
-        <option value="sans-serif" selected>
-          Sans Serif
-        </option>
-        <option value="serif">Serif</option>
-        <option value="monospace">Monospace</option>
-        <option value="Montserrat">Montserrat</option>
-      </select>
-      <button className="ql-bold">
-        <svg viewBox="0 0 18 18">
-          <path d="M7.5 4.5v9h-1v-9h1zM2 2h14v14H2V2z" />
-        </svg>
-      </button>
-      <button className="ql-italic">
-        <svg viewBox="0 0 18 18">
-          <path d="M7.5 4.5v9h-1v-9h1zM2 2h14v14H2V2z" />
-        </svg>
-      </button>
-      <ColorPicker color={color} changeColor={setColor} />
+    <div
+      className={clsx(
+        bg_container_no_padding,
+        'flex h-14 w-full justify-start gap-1 p-2 sm:p-3',
+      )}
+    >
+      {textEditorTools.map((section, index) => {
+        let toolSection = section.map(({ id, ToolIcon }, index) => {
+          const { realId, value } = {
+            realId: id.split('-')[0],
+            value: parseInt(id.split('-')[1]),
+          };
+          return (
+            <Button
+              id={id}
+              onPress={handleClick}
+              key={`${id}${index}`}
+              variant="light"
+              isIconOnly
+              size="sm"
+            >
+              <ToolIcon
+                className={
+                  (
+                    value
+                      ? selectionFormat[realId] === value
+                      : selectionFormat[realId]
+                  )
+                    ? 'h-5 w-5 stroke-f_accent'
+                    : 'h-5 w-5 stroke-default-500'
+                }
+              />
+            </Button>
+          );
+        });
+        toolSection.push(
+          <Divider key={`divider${index}`} orientation={`vertical`} />,
+        );
+        return toolSection;
+      })}
     </div>
   );
 }
