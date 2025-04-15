83c67438d6e7d81d0870a086f99a3dc75bb451b4
IciIcifur <iciicifur@yandex.ru>
2025-04-16 00:33:49 +0300

    Tiny fix
    ***
    frontend

--- frontend/app/(pages)/(main)/page.tsx
+++ frontend/app/(pages)/(main)/page.tsx
@@ -117,7 +117,7 @@ export default async function Main() {
       <div className="flex h-screen w-screen flex-col items-center justify-center gap-8 px-24 py-8">
         <div
           className={clsx(
-            'flex h-full min-h-0.5 w-full flex-col items-center justify-center',
+            'flex h-full min-h-56 w-full flex-col items-center justify-center',
             bg_container_no_padding,
             'rounded-bl-[64px] px-8 pt-8',
           )}
@@ -130,7 +130,7 @@ export default async function Main() {
         </div>
         <div
           className={clsx(
-            'flex h-80 w-full flex-col items-center justify-center',
+            'flex h-full max-h-72 min-h-52 w-full flex-col items-center justify-center',
             bg_container_no_padding,
             'rounded-tr-[64px] px-8 pt-8',
           )}
