9cdf19323b475109d39035debae5e5f0d1b4a819
IciIcifur <iciicifur@yandex.ru>
2025-04-23 21:44:17 +0300

    Added action buttons
    ***
    frontend

--- frontend/app/lib/components/projects/projectTree/boardCard.tsx
+++ frontend/app/lib/components/projects/projectTree/boardCard.tsx
@@ -6,6 +6,8 @@ import BoardIcon from '@/app/lib/components/menu/projectBar/boardIcon';
 import clsx from 'clsx';
 import { useRouter } from 'next/navigation';
 import { useActiveSectionContext } from '@/app/lib/components/projects/projectTree/projectTree';
+import { Button } from '@heroui/button';
+import { TrashIcon } from 'lucide-react';
 
 export default function BoardCard({
   parentList,
@@ -14,7 +16,7 @@ export default function BoardCard({
   parentList: string[];
   board: Pick<Board, 'id' | 'sectionId' | 'name' | 'type' | 'lastChange'>;
 }) {
-  const { activeNode, setActiveNode } = useActiveSectionContext();
+  const { activeNode, setActiveNode, removeNode } = useActiveSectionContext();
   const router = useRouter();
   const [isReadonly, setIsReadonly] = useState(true);
   const [boardName, setBoardName] = useState(board.name);
@@ -35,41 +37,54 @@ export default function BoardCard({
         )
       }
       style={{ paddingLeft: `${parentList.length * 16}px` }}
-      className="flex max-h-16 w-full items-center justify-between gap-2"
+      className="flex max-h-16 w-full items-center justify-between gap-2 p-1 pr-0"
     >
       <div
         onClick={() => setActiveNode({ id: board.id, parentList })}
         className={clsx(
-          'flex w-full cursor-pointer items-center justify-start gap-0 rounded-xl p-1 transition-colors hover:bg-default-100',
+          'group flex w-full cursor-pointer items-center justify-start gap-0 rounded-xl p-1 pr-0 transition-colors hover:bg-default-100',
           activeNode &&
             board.id === activeNode.id &&
             'bg-primary-100 hover:bg-primary-100',
         )}
       >
-        <div className="flex h-8 w-8 items-center justify-center">
-          <BoardIcon boardType={board.type} />
+        <div className="flex h-full w-full items-center">
+          <div className="flex h-8 w-8 items-center justify-center">
+            <BoardIcon boardType={board.type} />
+          </div>
+          <Input
+            isReadOnly={isReadonly}
+            autoFocus={!isReadonly}
+            onClick={(e) => e.stopPropagation()}
+            onFocus={() => setIsReadonly(false)}
+            onBlur={updateBoardName}
+            value={boardName}
+            onValueChange={setBoardName}
+            variant="bordered"
+            size="sm"
+            className={clsx(
+              'h-6 w-fit content-center rounded-md border-1.5 border-default/0',
+              !isReadonly && 'border-default-200',
+            )}
+            classNames={{
+              main: 'h-6',
+              mainWrapper: 'flex justify-center',
+              inputWrapper: 'shadow-none max-w-sm transition-all border-none',
+              input: 'truncate text-nowrap',
+            }}
+          />
         </div>
-        <Input
-          isReadOnly={isReadonly}
-          autoFocus={!isReadonly}
-          onClick={(e) => e.stopPropagation()}
-          onFocus={() => setIsReadonly(false)}
-          onBlur={updateBoardName}
-          value={boardName}
-          onValueChange={setBoardName}
-          variant="bordered"
-          size="sm"
-          className={clsx(
-            'h-6 w-fit content-center rounded-md border-1.5 border-default/0',
-            !isReadonly && 'border-default-200',
-          )}
-          classNames={{
-            main: 'h-6',
-            mainWrapper: 'flex justify-center',
-            inputWrapper: 'shadow-none max-w-sm transition-all border-none',
-            input: 'truncate text-nowrap',
-          }}
-        />
+        <div className="invisible flex h-full w-fit items-center justify-end gap-2 pr-2 group-hover:visible">
+          <Button
+            isIconOnly
+            onPress={() => removeNode(board.id, parentList)}
+            variant="light"
+            color="danger"
+            size="sm"
+          >
+            <TrashIcon className="stroke-danger-500" />
+          </Button>
+        </div>
       </div>
     </div>
   );
--- frontend/app/lib/components/projects/projectTree/subSectionCard.tsx
+++ frontend/app/lib/components/projects/projectTree/subSectionCard.tsx
@@ -3,7 +3,7 @@ import { useState } from 'react';
 import projectsStore from '@/app/stores/projectsStore';
 import clsx from 'clsx';
 import { Button } from '@heroui/button';
-import { ChevronRightIcon } from 'lucide-react';
+import { ChevronRightIcon, PlusIcon, TrashIcon } from 'lucide-react';
 import { Input } from '@heroui/input';
 import BoardCard from '@/app/lib/components/projects/projectTree/boardCard';
 import { useActiveSectionContext } from '@/app/lib/components/projects/projectTree/projectTree';
@@ -15,7 +15,8 @@ export default function SubSectionCard({
   parentList: string[];
   subSection: ProjectSection;
 }) {
-  const { activeNode, setActiveNode } = useActiveSectionContext();
+  const { activeNode, setActiveNode, addNode, removeNode } =
+    useActiveSectionContext();
   const [isReadonly, setIsReadonly] = useState(true);
   const [subSectionName, setSubSectionName] = useState(subSection.name);
   const [isExpanded, setIsExpanded] = useState(false);
@@ -32,53 +33,74 @@ export default function SubSectionCard({
     <div
       style={{ paddingLeft: `${parentList.length * 16}px` }}
       className={clsx(
-        'flex max-h-16 w-full flex-col items-start justify-start p-1 transition-all duration-500',
+        'flex max-h-16 w-full flex-col items-start justify-start p-1 pr-0 transition-all duration-500',
         isExpanded && 'max-h-[1000px]',
       )}
     >
       <div
         onClick={() => setActiveNode({ id: subSection.id, parentList })}
         className={clsx(
-          'flex w-full cursor-pointer items-center justify-start gap-0 rounded-xl p-1 hover:bg-default-100',
+          'group flex w-full cursor-pointer items-center justify-start gap-0 rounded-xl p-1 pr-0 hover:bg-default-100',
           activeNode &&
             subSection.id === activeNode.id &&
             'bg-primary-100 hover:bg-primary-100',
         )}
       >
-        <Button
-          isIconOnly
-          onPress={() => setIsExpanded(!isExpanded)}
-          variant="light"
-          size="sm"
-        >
-          <ChevronRightIcon
+        <div className="flex h-full w-full items-center">
+          <Button
+            isIconOnly
+            onPress={() => setIsExpanded(!isExpanded)}
+            variant="light"
+            size="sm"
+          >
+            <ChevronRightIcon
+              className={clsx(
+                'stroke-default-500 transition-transform',
+                isExpanded && 'rotate-90',
+              )}
+            />
+          </Button>
+          <Input
+            isReadOnly={isReadonly}
+            autoFocus={!isReadonly}
+            onClick={(e) => e.stopPropagation()}
+            onFocus={() => setIsReadonly(false)}
+            onBlur={updateSectionName}
+            value={subSectionName}
+            onValueChange={setSubSectionName}
+            variant="bordered"
+            size="sm"
             className={clsx(
-              'stroke-default-500 transition-transform',
-              isExpanded && 'rotate-90',
+              'h-6 w-fit content-center rounded-md border-1.5 border-default/0',
+              !isReadonly && 'border-default-200',
             )}
+            classNames={{
+              main: 'h-6',
+              mainWrapper: 'flex justify-center',
+              inputWrapper: 'shadow-none max-w-sm transition-all border-none',
+              input: 'truncate text-nowrap',
+            }}
           />
-        </Button>
-        <Input
-          isReadOnly={isReadonly}
-          autoFocus={!isReadonly}
-          onClick={(e) => e.stopPropagation()}
-          onFocus={() => setIsReadonly(false)}
-          onBlur={updateSectionName}
-          value={subSectionName}
-          onValueChange={setSubSectionName}
-          variant="bordered"
-          size="sm"
-          className={clsx(
-            'h-6 w-fit content-center rounded-md border-1.5 border-default/0',
-            !isReadonly && 'border-default-200',
-          )}
-          classNames={{
-            main: 'h-6',
-            mainWrapper: 'flex justify-center',
-            inputWrapper: 'shadow-none max-w-sm transition-all border-none',
-            input: 'truncate text-nowrap',
-          }}
-        />
+        </div>
+        <div className="invisible flex h-full w-fit items-center justify-end gap-2 pr-2 group-hover:visible">
+          <Button
+            isIconOnly
+            onPress={() => addNode([...parentList, subSection.id])}
+            variant="light"
+            size="sm"
+          >
+            <PlusIcon className="stroke-default-500" />
+          </Button>
+          <Button
+            isIconOnly
+            onPress={() => removeNode(subSection.id, parentList)}
+            variant="light"
+            color="danger"
+            size="sm"
+          >
+            <TrashIcon className="stroke-danger-500" />
+          </Button>
+        </div>
       </div>
       {isExpanded &&
         Array.from(subSection.children.values()).map((child) => {
