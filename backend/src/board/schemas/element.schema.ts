1ad873da8fcd58b3b842932c21263b37e556fce6
MINTCanella <veklebes@gmail.com>
2025-05-10 23:25:39 +0300

    FOGGY-21 Обновление схемы элементов

--- backend/src/board/schemas/element.schema.ts
+++ backend/src/board/schemas/element.schema.ts
@@ -31,6 +31,12 @@ export class BaseElement extends Document {
 
   @Prop({ required: true })
   strokeWidth: number;
+
+  @Prop({ required: true })
+  width: number;
+
+  @Prop({ required: true })
+  height: number;
 }
 
 export const BaseElementSchema = SchemaFactory.createForClass(BaseElement);
@@ -39,23 +45,13 @@ export const BaseElementSchema = SchemaFactory.createForClass(BaseElement);
 class RectElement extends BaseElement {
   @Prop({ required: true })
   cornerRadius: number;
-
-  @Prop({ required: true })
-  width: number;
-
-  @Prop({ required: true })
-  height: number;
 }
 
 export const RectElementSchema = SchemaFactory.createForClass(RectElement);
 
 @Schema()
 class EllipseElement extends BaseElement {
-  @Prop({ required: true })
-  width: number;
-
-  @Prop({ required: true })
-  height: number;
+  //BaseElement
 }
 
 export const EllipseElementSchema =
@@ -68,12 +64,6 @@ class TextElement extends BaseElement {
 
   @Prop({ required: true })
   content: string;
-
-  @Prop({ required: true })
-  width: number;
-
-  @Prop({ required: true })
-  height: number;
 }
 
 export const TextElementSchema = SchemaFactory.createForClass(TextElement);
@@ -84,7 +74,13 @@ class LineElement extends BaseElement {
   points: number[];
 
   @Prop({ required: true })
-  width: number;
+  tension: number;
+
+  @Prop({ required: true, enum: ['miter', 'round', 'bevel'] })
+  lineJoin: 'miter' | 'round' | 'bevel';
+
+  @Prop({ required: true, enum: ['butt', 'round', 'square'] })
+  lineCap: 'butt' | 'round' | 'square';
 }
 
 export const LineElementSchema = SchemaFactory.createForClass(LineElement);
@@ -93,9 +89,6 @@ export const LineElementSchema = SchemaFactory.createForClass(LineElement);
 class MarkerElement extends BaseElement {
   @Prop({ required: true })
   points: number[];
-
-  @Prop({ required: true })
-  width: number;
 
   @Prop({ required: true })
   opacity: number;
