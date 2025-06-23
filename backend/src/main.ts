7ae5653bd5152075cc3e5146748a660625612865
MINTCanella <veklebes@gmail.com>
2025-06-23 23:05:37 +0300

    Hotfix

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -34,47 +34,49 @@ async function bootstrap() {
     allowedHeaders: ['x-api-key', 'x-user-id'],
   });
 
-  const config = new DocumentBuilder()
-    .setTitle('Foggy API Documentation')
-    .setDescription('The API description')
-    .setVersion('1.0')
-    .addApiKey(
-      {
-        type: 'apiKey',
-        name: 'x-api-key',
-        in: 'header',
-        description: 'API Key for external services',
-      },
-      'x-api-key',
-    )
-    .addApiKey(
-      {
-        type: 'apiKey',
-        name: 'x-user-id',
-        in: 'header',
-        description: 'User ID for authentication',
-      },
-      'x-user-id',
-    )
-    .addSecurityRequirements('x-api-key', [])
-    .addSecurityRequirements('x-user-id', [])
-    .build();
+  if (configService.get<string>('NODE_ENV') !== 'production') {
+    const config = new DocumentBuilder()
+      .setTitle('Foggy API Documentation')
+      .setDescription('The API description')
+      .setVersion('1.0')
+      .addApiKey(
+        {
+          type: 'apiKey',
+          name: 'x-api-key',
+          in: 'header',
+          description: 'API Key for external services',
+        },
+        'x-api-key',
+      )
+      .addApiKey(
+        {
+          type: 'apiKey',
+          name: 'x-user-id',
+          in: 'header',
+          description: 'User ID for authentication',
+        },
+        'x-user-id',
+      )
+      .addSecurityRequirements('x-api-key', [])
+      .addSecurityRequirements('x-user-id', [])
+      .build();
 
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document, {
-    swaggerOptions: {
-      apisSorter: 'alpha',
-      tagsSorter: 'order',
-      filter: true,
-      persistAuthorization: true,
-      operationsSorter: (a, b) => {
-        const order = ['post', 'get', 'patch', 'put', 'delete'];
-        const methodA = a.get('method');
-        const methodB = b.get('method');
-        return order.indexOf(methodA) - order.indexOf(methodB);
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document, {
+      swaggerOptions: {
+        apisSorter: 'alpha',
+        tagsSorter: 'order',
+        filter: true,
+        persistAuthorization: true,
+        operationsSorter: (a, b) => {
+          const order = ['post', 'get', 'patch', 'put', 'delete'];
+          const methodA = a.get('method');
+          const methodB = b.get('method');
+          return order.indexOf(methodA) - order.indexOf(methodB);
+        },
       },
-    },
-  });
+    });
+  }
 
   await app.listen(configService.get<number>('BACKEND_PORT'));
 }
