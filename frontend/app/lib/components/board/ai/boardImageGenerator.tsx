3a7721a187b22958dae530911e9bcfff16e66d25
Olesya Dobrovolskaya <130292278+IciIcifur@users.noreply.github.com>
2026-02-17 19:47:21 +0300

    Foggy 73 (#61)
    * Working on save board image
    
    ***
    frontend
    
    * Working on save board image
    
    ***
    frontend
    
    * Working on save board image
    
    ***
    frontend
    
    * Working on save board image
    
    ***
    frontend
    
    * Image is generated with background
    
    ***
    frontend
    
    * Image is uploaded to cloud
    
    ***
    frontend
    
    * Image is uploaded to cloud
    
    ***
    frontend
    
    * Small fixes
    
    ***
    frontend

---
+++ frontend/app/lib/components/board/ai/boardImageGenerator.tsx
@@ -0,0 +1,91 @@
+'use client';
+
+import { CloudUploadIcon } from 'lucide-react';
+import { Button } from '@heroui/button';
+import React, { useState } from 'react';
+import useAdaptiveParams from '@/app/lib/hooks/useAdaptiveParams';
+import { foggy_accent } from '@/tailwind.config';
+import { useBoardContext } from '@/app/lib/components/board/boardContext';
+import GetBoardImage from '@/app/lib/utils/getBoardImage';
+import boardStore from '@/app/stores/boardStore';
+import { useTheme } from 'next-themes';
+import { addToast } from '@heroui/toast';
+import settingsStore from '@/app/stores/settingsStore';
+import { uploadImage } from '@/app/lib/server/actions/handleImage';
+import { CopyToClipboard } from '@/app/lib/utils/copyToClipboard';
+import FTooltip from '@/app/lib/components/foggyOverrides/fTooltip';
+import { observer } from 'mobx-react-lite';
+
+const BoardImageGenerator = observer(() => {
+  const { resolvedTheme } = useTheme();
+  const { commonSize } = useAdaptiveParams();
+  const { stageRef } = useBoardContext();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleUpload = async () => {
+    if (!boardStore.activeBoard) return;
+
+    setIsLoading(true);
+    let blob = null;
+    try {
+      blob = await GetBoardImage(
+        stageRef,
+        boardStore.activeBoard.layers,
+        resolvedTheme === 'light' ? '#e4e4e7' : '#27272a',
+      );
+      if (!blob) throw new Error();
+    } catch (e: any) {
+      addToast({
+        color: 'danger',
+        severity: 'danger',
+        title: settingsStore.t.toasts.board.generateBoardImageError,
+        description: e.message || '',
+      });
+      setIsLoading(false);
+      return;
+    }
+
+    try {
+      const result = await uploadImage(
+        'board_images',
+        blob,
+        'board_temp_image_',
+        { type: 'hash', base: boardStore.activeBoard.id },
+      );
+      if ('error' in result) throw new Error(result.error);
+
+      addToast({
+        color: 'success',
+        severity: 'success',
+        title: settingsStore.t.toasts.board.uploadBoardImageSuccess,
+      });
+      await CopyToClipboard(result.url);
+    } catch (e: any) {
+      addToast({
+        color: 'danger',
+        severity: 'danger',
+        title: settingsStore.t.toasts.board.uploadBoardImageError,
+        description: e.message || '',
+      });
+    }
+    setIsLoading(false);
+  };
+
+  return (
+    <FTooltip placement="right" content={settingsStore.t.toolTips.uploadButton}>
+      <Button
+        data-testid="save-board-image-btn"
+        onPress={handleUpload}
+        isIconOnly
+        isLoading={isLoading}
+        color={foggy_accent.light.DEFAULT as any}
+        size={commonSize}
+        className="accent-sh absolute bottom-3 left-3 z-50 font-semibold sm:bottom-6 sm:left-6"
+      >
+        <CloudUploadIcon />
+      </Button>
+    </FTooltip>
+  );
+});
+
+export default BoardImageGenerator;
