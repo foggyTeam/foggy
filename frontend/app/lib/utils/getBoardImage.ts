d0fe1cc7cc96203ac94e461ae25f71602b009599
IciIcifur <iciicifur@yandex.ru>
2026-02-17 15:59:38 +0300

    Working on save board image
    ***
    frontend

--- frontend/app/lib/utils/getBoardImage.ts
+++ frontend/app/lib/utils/getBoardImage.ts
@@ -21,26 +21,27 @@ function getSaveConfig(stage: Stage, layers: BoardElement[][]) {
       )
         continue;
 
+      const x = element.x;
+      const y = element.y;
+
       if (element.type === 'line') {
         for (let i = 0; i < element.points.length; i++) {
           if (i % 2) {
-            minY = min(minY, element.points[i]);
-            maxY = max(maxY, element.points[i]);
+            minY = min(minY, y + element.points[i]);
+            maxY = max(maxY, y + element.points[i]);
           } else {
-            minX = min(minX, element.points[i]);
-            maxX = max(maxX, element.points[i]);
+            minX = min(minX, x + element.points[i]);
+            maxX = max(maxX, x + element.points[i]);
           }
         }
         continue;
       }
 
-      const x = element.x;
-      const y = element.y;
       minX = min(minX, x);
       minY = min(minY, y);
 
       maxX = max(maxX, x + element.width);
-      maxY = max(maxY, x + element.height);
+      maxY = max(maxY, y + element.height);
     }
   }
 
@@ -56,18 +57,16 @@ function getSaveConfig(stage: Stage, layers: BoardElement[][]) {
   const stageY = stage.y();
   const scale = stage.scaleX();
 
-  console.log(stageX, stageY, scale);
-  const relativeX = (minX - -stageX) * scale;
-  const relativeY = (minY - -stageY) * scale;
-  const relativeWidth = width * scale;
-  const relativeHeight = height * scale;
+  const relativeMinX = (minX - stageX) / scale;
+  const relativeMaxX = (maxX - stageX) / scale;
+  const relativeMinY = (stageY - minY) / scale;
+  const relativeMaxY = (stageY - maxY) / scale;
 
-  console.log(relativeX, relativeY);
   return {
-    x: relativeX,
-    y: relativeY,
-    width: relativeWidth,
-    height: relativeHeight,
+    x: relativeMinX,
+    y: relativeMinY,
+    width: Math.abs(relativeMaxX - relativeMinX),
+    height: Math.abs(relativeMaxY - relativeMinY),
     pixelRatio: pixelRatio(),
     mimeType: 'image/jpeg',
   };
@@ -87,7 +86,6 @@ export default async function GetBoardImage(
     const blob: Blob | any = await stageRef.current?.toBlob(config);
     if (!blob) throw new Error();
 
-    console.log(blob);
     return blob;
   } catch (e: any) {
     console.error('Failed to create board blob');
