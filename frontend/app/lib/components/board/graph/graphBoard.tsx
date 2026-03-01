080778ad203737fe190143f63bbf941dcdde1fa1
IciIcifur <iciicifur@yandex.ru>
2026-03-01 16:07:28 +0300

    Fixed types
    ***
    frontend

--- frontend/app/lib/components/board/graph/graphBoard.tsx
+++ frontend/app/lib/components/board/graph/graphBoard.tsx
@@ -2,20 +2,21 @@
 
 import '@xyflow/react/dist/style.css';
 import { observer } from 'mobx-react-lite';
+import type { Edge, Node } from '@xyflow/react';
 import { Background, Controls, ReactFlow } from '@xyflow/react';
 import graphBoardStore from '@/app/stores/board/graphBoardStore';
+import { computed } from 'mobx';
 
 const GraphBoard = observer(() => {
+  const nodes = computed(() => graphBoardStore.boardNodes as Node[]);
+  const edges = computed(() => graphBoardStore.boardEdges as Edge[]);
+
   return (
     <div
       data-testid="graph-board"
       className="relative h-full w-full overflow-hidden"
     >
-      <ReactFlow
-        nodes={graphBoardStore.boardNodes}
-        edges={graphBoardStore.boardEdges}
-        fitView
-      >
+      <ReactFlow nodes={nodes.get()} edges={edges.get()} fitView>
         <Background />
         <Controls />
       </ReactFlow>
