ca13fa89eb0068ad888d9f58ddd3756dedc94b05
IciIcifur <iciicifur@yandex.ru>
2026-04-05 13:45:05 +0300

    Starting a new branch
    ***
    frontend

--- frontend/app/(pages)/(main)/project/[project_id]/[section_id]/[board_id]/simple/page.tsx
+++ frontend/app/(pages)/(main)/project/[project_id]/[section_id]/[board_id]/simple/page.tsx
@@ -1,6 +1,5 @@
-import React from 'react';
 import BoardClientWrapper from '@/app/lib/components/board/simple/boardClientWrapper';
 
-export default async function SimpleBoardPage() {
+export default function SimpleBoardPage() {
   return <BoardClientWrapper />;
 }
