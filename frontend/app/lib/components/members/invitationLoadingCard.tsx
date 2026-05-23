51bfe50a6c14bfd87d682014681e199b308285a2
Olesya Dobrovolskaya <130292278+IciIcifur@users.noreply.github.com>
2026-05-23 14:50:59 +0300

    FOGGY-61 (#97)

--- frontend/app/lib/components/members/invitationLoadingCard.tsx
+++ frontend/app/lib/components/members/invitationLoadingCard.tsx
@@ -4,45 +4,75 @@ import { Progress } from '@heroui/progress';
 import clsx from 'clsx';
 import { bg_container } from '@/app/lib/types/styles';
 import FoggySmall from '@/app/lib/components/svg/foggySmall';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { ProcessInvitationToken } from '@/app/lib/server/actions/membersServerActions';
 import { useRouter } from 'next/navigation';
 import { addToast } from '@heroui/toast';
 import settingsStore from '@/app/stores/settingsStore';
+import { Project, Team } from '@/app/lib/types/definitions';
+import ProjectRequest from '@/app/lib/components/projects/projectRequest';
+import TeamRequest from '@/app/lib/components/teams/teamRequest';
+import { FButton } from '@/app/lib/components/foggyOverrides/fButton';
+import useAdaptiveParams from '@/app/lib/hooks/useAdaptiveParams';
+import userStore from '@/app/stores/userStore';
 
-export default function InvitationLoadingCard({ token }: { token: string }) {
+export default function InvitationLoadingCard({
+  entityType,
+  token,
+}: {
+  entityType: 'project' | 'team';
+  token: string;
+}) {
+  const { commonSize } = useAdaptiveParams();
   const router = useRouter();
+  const [isLoading, setIsLoading] = useState(true);
+  const [requiredEntity, setRequiredEntity] = useState<
+    Partial<Project> | Partial<Team>
+  >();
+
+  async function acceptInvitation() {
+    if (!requiredEntity) return;
+
+    setIsLoading(true);
+    try {
+      const result = await ProcessInvitationToken(entityType, token, true);
+      if (typeof result !== 'string')
+        throw new Error(result.errors[0].toString());
+
+      addToast({
+        color: 'success',
+        severity: 'success',
+        title:
+          settingsStore.t.toasts.members.invitationLink.accepted[entityType],
+      });
+      router.push(`/${entityType}/${requiredEntity.id}`);
+    } catch (e: any) {
+      addToast({
+        color: 'danger',
+        severity: 'danger',
+        title: settingsStore.t.toasts.members.invitationLink.error.title,
+        description: e.toString(),
+      });
+    }
+    setIsLoading(false);
+  }
+
   useEffect(() => {
-    // TODO: consider decryption type
-    ProcessInvitationToken(token)
-      .then(
-        (result: {
-          accepted: boolean;
-          type: 'team' | 'project';
-          id: string;
-        }) => {
-          if (result.accepted) {
-            addToast({
-              color: 'success',
-              severity: 'success',
-              title:
-                settingsStore.t.toasts.members.invitationLink.accepted[
-                  result.type
-                ],
-            });
-            router.push(`/${result.type}/${result.id}`);
-          } else {
-            addToast({
-              color: 'warning',
-              severity: 'warning',
-              title: settingsStore.t.toasts.members.invitationLink.error.title,
-              description:
-                settingsStore.t.toasts.members.invitationLink.error.description,
-            });
-            router.push(`/${result.type}/request/${result.id}`);
+    ProcessInvitationToken(entityType, token)
+      .then((result: Partial<Team> | Partial<Project>) => {
+        for (const member of result.members || [])
+          if (member.id === userStore.user?.id) {
+            router.push(`/${entityType}/${result.id}`);
+            return;
           }
-        },
-      )
+
+        setRequiredEntity(
+          Object.assign(result, {
+            settings: { ...result.settings, allowRequests: false },
+          }),
+        );
+        setIsLoading(false);
+      })
       .catch((e: any) => {
         addToast({
           color: 'danger',
@@ -50,36 +80,57 @@ export default function InvitationLoadingCard({ token }: { token: string }) {
           title: settingsStore.t.toasts.members.invitationLink.error.title,
           description: e.toString(),
         });
-        router.push('/');
+        router.push(`/`);
       });
   }, []);
 
   return (
-    <div className="bg-default-900/10 absolute top-0 left-0 z-30 flex h-full w-full items-center justify-center backdrop-blur-xl">
+    <div className="bg-background/50 absolute top-0 left-0 z-30 flex h-full w-full items-center justify-center backdrop-blur-xl">
       <div
         className={clsx(
-          'flex h-fit w-full max-w-sm flex-col items-center justify-center gap-4',
+          'flex h-fit w-full flex-col items-center justify-center gap-4 transition-all',
           bg_container,
           'px-12',
+          isLoading ? 'max-w-sm' : 'max-w-xl',
         )}
       >
-        <div className="flex h-fit w-full flex-col items-center justify-center gap-4">
-          <FoggySmall
-            width={128}
-            height={128}
-            alt={'foggy logo'}
-            className="stroke-primary fill-[url(#logo-gradient)] stroke-0 transition-all duration-300"
-          />
-          <Progress
-            size="md"
-            classNames={{
-              indicator: 'bg-linear-to-r from-primary-500 to-danger-400',
-            }}
-            isIndeterminate
-            aria-label="Loading..."
-            className="w-full"
-          />
-        </div>
+        {isLoading ? (
+          <div className="flex h-fit w-full flex-col items-center justify-center gap-4 px-4">
+            <FoggySmall
+              width={128}
+              height={128}
+              alt={'foggy logo'}
+              withGradient
+              className="stroke-primary stroke-0 transition-all duration-300"
+            />
+            <Progress
+              size="md"
+              classNames={{
+                indicator: 'bg-linear-to-r from-primary-500 to-danger-400',
+              }}
+              isIndeterminate
+              aria-label="Loading..."
+              className="w-full"
+            />
+          </div>
+        ) : (
+          <div className="flex h-fit w-full flex-col items-center justify-center gap-4 px-4">
+            {entityType === 'project' ? (
+              <ProjectRequest project={requiredEntity as Project} />
+            ) : (
+              <TeamRequest team={requiredEntity as Team} />
+            )}
+            <FButton
+              size={commonSize}
+              color="primary"
+              variant="solid"
+              className="w-full"
+              onPress={acceptInvitation}
+            >
+              {settingsStore.t.members.addMember.acceptInvitation[entityType]}
+            </FButton>
+          </div>
+        )}
       </div>
     </div>
   );
